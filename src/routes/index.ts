2d26ab5d53fcad52b036f96d85c644d30da97ef2
santiago <a01255853@tec.mx>
2026-03-06 17:31:26 -0700

    feat(empresa-miembro): add REST routes and mount router in api index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router, Request, Response } from 'express';
 import productRoutes from './productRoutes';  
+import empresaMiembroRoutes from './empresaMiembroRoutes';
 
 const apiRouter:Router = Router();  
 
 apiRouter.use('/product', productRoutes);  
+apiRouter.use('/empresa-miembro', empresaMiembroRoutes);
 
 apiRouter.get('/', (req:Request, res: Response) => {  
 res.send('que pedo!')  
